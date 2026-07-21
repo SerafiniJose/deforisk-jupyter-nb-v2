e231299c232dfd849733a00b0844e0a31b759e4f
SerafiniJose <jose.serafinig@gmail.com>
2026-07-21 11:45:03 +0200

    i18n: add workflow.step_badge key for unified step header

--- tests/test_workflow_steps.py
+++ tests/test_workflow_steps.py
@@ -144,5 +144,5 @@ def test_registry_keys_resolve_in_both_locales():
                 assert spec.count_key_one in keys, (lang, spec.key)
                 assert spec.count_key_other in keys, (lang, spec.key)
         for extra in ("workflow.step_position", "workflow.all_steps",
-                      "workflow.count_empty"):
+                      "workflow.count_empty", "workflow.step_badge"):
             assert extra in keys, (lang, extra)
